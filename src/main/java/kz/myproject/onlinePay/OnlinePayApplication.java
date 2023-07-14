9cf23c134b8d7610196309b32448fdfde5f124ba
Olzhas Kurmangaliev <olzhaskurmangaliev335@gmail.com>
2023-07-14 23:40:27 +0600

    initialize

---
+++ src/main/java/kz/myproject/onlinePay/OnlinePayApplication.java
@@ -0,0 +1,13 @@
+package kz.myproject.onlinePay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/myproject/onlinePay/OnlinePayApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.myproject.onlinePay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
